6bde6a140c3dda71d7da0c67f15542b29d25fc53
Majki1234 <wwsis4306@wp.pl>
2016-04-10 16:09:39 +0200

    Aktualizacja

--- main.cpp
+++ main.cpp
@@ -5,5 +5,8 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
+	for(int i=0;i<10;++i){
+    		cout<<"O kurwa, ale faza!!!!!!!!!!11111oneoneone."<<endl;
+	}
     return 0;
 }
